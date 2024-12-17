931b3797fc16a475c230aae86907de5f67896c29
ANDREA VALENTE <valente.andrea.08@itisdalmine.edu.it>
2024-12-17 09:36:02 +0100

    implementazione base della calcolatrice

--- calcolatrice.py
+++ calcolatrice.py
@@ -3,7 +3,6 @@ while(scelta!=0):
     print("Benvenuto nella nostra calcolatrice")
     print("Inserisci l'operazione che vuoi effettuare")
     scelta =int(input("1)Sottrazione\n2)Addizzione\n0)Exit\n"))
-
     if scelta==1:
         n_1 =int(input("Inserisci il primo numero"))
         n_2 =int(input("Inserisci il secondo numero"))
