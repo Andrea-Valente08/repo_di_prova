4185dd329cb7293f12bdfab59064a50dee74c71a
ANDREA VALENTE <valente.andrea.08@itisdalmine.edu.it>
2024-12-17 10:01:37 +0100

    implementazione del menu

--- Menu.py
+++ Menu.py
@@ -1,1 +1,15 @@
-print("hello")
+import calcolatrice as clc
+
+while True:
+    print("Benvenuto nella nostra calcolatrice")
+    print("Inserisci l'operazione che vuoi effettuare")
+    scelta =int(input("1)Sottrazione\n2)Addizzione\n0)Exit\n"))
+    if scelta==0:
+        print("Grazie per aver usato la nostra calcolatrice, a presto!")
+        break
+    if scelta==1:
+        clc.Sottrazzione()
+    elif scelta==2:
+        clc.Somma()
+    else:
+        print("Scelta non corretta!")
