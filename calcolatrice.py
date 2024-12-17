e2b712c1dc4a1d3a6c1308c3e6018cd017820666
ANDREA VALENTE <valente.andrea.08@itisdalmine.edu.it>
2024-12-17 09:52:11 +0100

    implementazione base della calcolatrice

--- calcolatrice.py
+++ calcolatrice.py
@@ -2,7 +2,7 @@ def Somma():
     n_1 =int(input("Inserisci il primo numero"))
     n_2 =int(input("Inserisci il secondo numero"))
     print(f"{n_1} + {n_2} = {n_1 + n_2}")
-def Sottrazzione:
+def Sottrazzione():
     n_1 =int(input("Inserisci il primo numero"))
     n_2 =int(input("Inserisci il secondo numero"))
     print(f"{n_1} - {n_2} = {n_1 - n_2}")
