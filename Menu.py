8d78ee96c15d3a6908334a7da6a61d0a3d6bcc26
Andrea-Valente08 <Valente.Andrea.08@itisdalmine.edu.it>
2024-12-17 09:57:37 +0100

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -1,1 +1,13 @@
-print("hello")
+while(true):
+    print("Benvenuto nella nostra calcolatrice")
+    print("Inserisci l'operazione che vuoi effettuare")
+    scelta =int(input("1)Sottrazione\n2)Addizzione\n0)Exit\n"))
+    if scelta==0:
+        print("Grazie per aver usato la nostra calcolatrice, a presto!")
+        break
+    if scelta==1:
+        Sottrazione()
+    elif scelta==2:
+        Somma()
+    else:
+        print("Scelta non corretta!")
